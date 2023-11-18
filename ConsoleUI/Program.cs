571f9084cbe27976df1c2b53f0bf8329db2d5132
iloozealot <iloozealot@gmail.com>
2023-11-18 17:47:39 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+////RunsALot();
+
+////Console.ReadLine();
+
+////static void RunsALot()
+////{
+////    long total = 0;
+////    int test = 0;
+
+////    for (int i = -1000; i < 1000; i++)
+////    {
+////        total += i;
+////        try
+////        {
+////            test = 5 / i;
+////        }
+////        catch
+////        {
+////            Console.WriteLine("There was an exception.");
+////        }
+////    }
+
+////    Console.WriteLine($"The total is { total }");
+//}
+
+
+for (int i = 1; i < 101; i++)
+{
+    Console.WriteLine(i);
+
+    if (i == 73)
+    {
+        throw new Exception();
+    }
+}
+
+Console.ReadLine();
